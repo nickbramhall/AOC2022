253f849d982bef33a9f2437168aab2685db8aba6
Nick Bramhall <n.j.bramhall@gmail.com>
2022-12-04 17:32:19 +0000

    Updating day4 solution

--- day4.py
+++ day4.py
@@ -20,26 +20,12 @@ count=0
 
 for line in all_lines:
     elf1_start, elf1_stop, elf2_start, elf2_stop = get_elf_ranges(line)
-    if elf1_start == elf1_stop:
-        if elf1_start == elf2_start or elf1_start == elf2_stop:
-            # print(f'Match found for {elf1},{elf2}')
-            count = count + 1
-            continue
-    if elf2_start == elf2_stop:
-        if elf2_start == elf1_start or elf2_start == elf1_stop:
-            # print(f'Match found for {elf1},{elf2}')
-            count = count + 1
-            continue
-    if elf1_start <= elf2_start:
-        if elf1_stop >= elf2_stop:
-            # print(f'Match found for {elf1},{elf2}')
-            count = count + 1
-            continue
-    if elf1_start >= elf2_start:
-        if elf1_stop <= elf2_stop:
-            # print(f'Match found for {elf1},{elf2}')
-            count = count + 1
-            continue
+    if elf1_start <= elf2_start and elf1_stop >= elf2_stop:
+        count = count + 1
+        continue
+    if elf2_start <= elf1_start and elf2_stop >= elf1_stop:
+        count = count + 1
+        continue
     # print('\n')
     # print('.'*(elf1_start-1)+'X'*(elf1_stop-elf1_start+1)+'.'*(100-elf1_stop))
     # print('.'*(elf2_start-1)+'X'*(elf2_stop-elf2_start+1)+'.'*(100-elf2_stop))

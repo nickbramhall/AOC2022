7a6e51550f9e18306ebcc06ffe426118e8cde820
Nick Bramhall <n.j.bramhall@gmail.com>
2022-12-08 09:51:55 +0000

    Tidying up Day 8 solution

--- day8.py
+++ day8.py
@@ -11,11 +11,10 @@ newLineList = [line.rstrip('\n') for line in open(input_file)]
 total_cols = len(newLineList[0])
 total_rows = len(newLineList)
 
-edge_counter = total_cols*2 + total_rows*2 - 4
-interior_visible_counter=0
+visible_counter = total_cols*2 + total_rows*2 - 4
 
 search_results={}
-scenic_score_results={}
+scenic_score_result=0
 
 for start_row in range(1,total_rows-1,1):
 
@@ -26,7 +25,6 @@ for start_row in range(1,total_rows-1,1):
         # print(f'Check tree at {start_row},{start_col} : {tree_value}')
 
         # walk up
-
         row=start_row-1
         col=start_col
 
@@ -40,14 +38,9 @@ for start_row in range(1,total_rows-1,1):
                 up_counter = up_counter + 1
             if search_tree >= tree_value:
                 up_visible = False
-                # continue
-            
             row=row-1
-        
-        # print(up_visible)
 
         # walk down
-
         row=start_row+1
         col=start_col
 
@@ -61,12 +54,9 @@ for start_row in range(1,total_rows-1,1):
                 down_counter = down_counter + 1
             if search_tree >= tree_value:
                 down_visible = False
-                # continue
-            
             row=row+1
 
         # walk left
-
         row=start_row
         col=start_col-1
 
@@ -79,12 +69,9 @@ for start_row in range(1,total_rows-1,1):
                 left_counter = left_counter + 1
             if search_tree >= tree_value:
                 left_visible = False
-                # continue
-            
             col=col-1
 
         # walk right
-
         row=start_row
         col=start_col+1
 
@@ -97,27 +84,19 @@ for start_row in range(1,total_rows-1,1):
                 right_counter = right_counter + 1
             if search_tree >= tree_value:
                 right_visible = False
-                # continue
-            
             col=col+1
 
+        # Part 1 - updating visible counter if visibility is ok in at least one direction
         if up_visible is True or down_visible is True or left_visible is True or right_visible is True:
             search_results[(start_row,start_col)]=True
-            interior_visible_counter=interior_visible_counter+1
+            visible_counter=visible_counter+1
         else:
             search_results[(start_row,start_col)]=False
-
-        # print(f'up: {up_counter} - down: {down_counter} - left:{left_counter} - right:{right_counter}')
-
+        
+        # Part 2 - updating scenic score if it beats the current highest score
         scenic_score= up_counter * down_counter * left_counter * right_counter
-        scenic_score_results[(start_row,start_col)]=scenic_score
+        if scenic_score > scenic_score_result:
+            scenic_score_result = scenic_score
 
-total_visible_counter=edge_counter+interior_visible_counter
-print(f'Part 1: {total_visible_counter}')
-
-# Part 2
-
-sorted_size = {k: v for k, v in sorted(scenic_score_results.items(), key=lambda item: item[1])}
-last_key = list(sorted_size)[-1]
-
-print(f'Part 2: {scenic_score_results[last_key]}')
+print(f'Part 1: {visible_counter}')
+print(f'Part 2: {scenic_score_result}')
